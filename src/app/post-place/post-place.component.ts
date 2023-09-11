3981050d8419fcda3fd87bcd52b775c295084943
klemenslatnar <klemen.slatnar06@gmail.com>
2023-09-11 10:01:46 +0200

    Post place form html validation added

--- src/app/post-place/post-place.component.ts
+++ src/app/post-place/post-place.component.ts
@@ -8,17 +8,30 @@ import { UtilService } from '../util.service';
   styleUrls: ['./post-place.component.css'],
 })
 export class PostPlaceComponent {
-  placeName?: string;
-  coordinates?: string; // oblika : "lat, lon"
-  imageUrl?: string;
-  description?: string;
+  budgetValues = [1, 2, 3];
+
+  placeName: string = '';
+  coordinates: string = ''; // oblika : "lat, lon"
+  imageUrl: string = '';
+  description: string = '';
   region = ['Gorenjska']; // regije še pridejo
-  budget = [1, 2, 3];
-  attractions = [];
+  budget: number = 1;
+  attraction: string = '';
+  attractions: string[] = [];
 
   constructor(private backend: BackendService, private util: UtilService) {}
 
   setBudget(value: number): string {
     return this.util.setBudget(value);
   }
+
+  addAttraction(): void {
+    const attraction = this.attraction;
+    this.attractions.push(attraction);
+    this.attraction = '';
+  }
+
+  addPlace(): void {}
+
+  onSubmit() {}
 }
